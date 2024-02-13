f5ca8f8859716b8f9ce213c997389d3afe850f4b
vladislavkleiman <kleimanvladislav@gmail.com>
2024-02-13 21:09:55 +0200

    asgsdhbs

--- client/server.js
+++ client/server.js
@@ -1,6 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname);
 
 const app = express();
 app.use(cors());
@@ -9,8 +16,8 @@ app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on ${process.env.PORT || 3000}`);
 });
 
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./dist", "index.html"));
 });
