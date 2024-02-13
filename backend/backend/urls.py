9c0c41804de25d8773174b376a88546898ae40f6
vladislavkleiman <kleimanvladislav@gmail.com>
2024-02-13 20:31:31 +0200

    asgfASG

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from website_deploy.views import health_check
+from backend.views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
